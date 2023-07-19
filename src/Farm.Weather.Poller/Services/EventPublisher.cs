fa9faa44a9c7322b299f53b0f8860f737e2b0946
Slava Melnichenko <melnichenkoviatcheslav@gmail.com>
2023-07-19 21:59:20 +0300

    improve publisher and cron

--- src/Farm.Weather.Poller/Services/EventPublisher.cs
+++ src/Farm.Weather.Poller/Services/EventPublisher.cs
@@ -5,26 +5,32 @@
 // without the prior written consent of the copyright owner.
 // </copyright>
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Farm.Weather.Contracts.IntegrationEvents;
+using Farm.Weather.Poller.Options;
 using MassTransit;
+using Microsoft.Extensions.Options;
 
 namespace Farm.Weather.Poller.Services;
 
 public class EventPublisher : IEventPublisher
 {
+    private readonly Uri _queue;
     private readonly IBus _bus;
 
-    public EventPublisher(IBus bus)
+    public EventPublisher(IBus bus, IOptions<CommandBusOptions> options)
     {
         _bus = bus;
+        _queue = new Uri($"queue:{options.Value.WeatherDataQueue}");
     }
 
     public async Task SendIntegrationCommandAsync(
         IWeatherRawDataCreated[] integrationEvents,
         CancellationToken cancellationToken)
     {
-        await _bus.PublishBatch(integrationEvents, cancellationToken);
+        var endpoint = await _bus.GetSendEndpoint(_queue);
+        await endpoint.SendBatch(integrationEvents, cancellationToken);
     }
 }
